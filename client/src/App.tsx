aa044a0eb7d69ee468032406d7f64160aa96cd2e
Timothe Calvin <tcalv976@gmail.com>
2025-09-04 12:02:52 -0400

    resolving conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,6 +38,7 @@ function App() {
 
   return (
     <main className="flex flex-col items-center min-h-screen p-6 bg-gray-50">
+
       <div className="w-full mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {cards.map((card, idx) => (
